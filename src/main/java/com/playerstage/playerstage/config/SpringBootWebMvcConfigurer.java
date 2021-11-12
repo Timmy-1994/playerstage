876c34f0be6e986c5788aecbdcc196732d0f7eeb
timmy <>
2021-11-12 16:29:12 +0800

    feat: serve directory for SPA

--- src/main/java/com/playerstage/playerstage/config/SpringBootWebMvcConfigurer.java
+++ src/main/java/com/playerstage/playerstage/config/SpringBootWebMvcConfigurer.java
@@ -5,13 +5,21 @@ import java.nio.file.Paths;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
 public class SpringBootWebMvcConfigurer implements WebMvcConfigurer {
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        URI uri = Paths.get("upload").toUri();
-        registry.addResourceHandler("/files/**").addResourceLocations(uri.toString());
+        registry.addResourceHandler("/files/**").addResourceLocations(Paths.get("upload").toUri().toString());
+        registry.addResourceHandler("/**").addResourceLocations(Paths.get("frontend").toUri().toString());
     }
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
+
 }
