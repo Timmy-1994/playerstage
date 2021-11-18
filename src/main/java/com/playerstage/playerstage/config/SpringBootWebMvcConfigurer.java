b40aa603d316c203d590952a05df23d4f003163f
timmy <>
2021-11-18 11:44:11 +0800

    fix: SPA resource & cicd gradle task

--- src/main/java/com/playerstage/playerstage/config/SpringBootWebMvcConfigurer.java
+++ src/main/java/com/playerstage/playerstage/config/SpringBootWebMvcConfigurer.java
@@ -6,8 +6,12 @@ import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
 import org.apache.tomcat.util.http.SameSiteCookies;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -45,13 +49,26 @@ public class SpringBootWebMvcConfigurer implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/files/**").addResourceLocations(Paths.get("upload").toUri().toString());
-        registry.addResourceHandler("/**").addResourceLocations(Paths.get("frontend").toUri().toString());
+        registry
+            .addResourceHandler("/**", "/files/**")
+            .addResourceLocations(
+                Paths.get("frontend").toUri().toString(),
+                Paths.get("upload").toUri().toString()
+            )
+            .setCachePeriod(3600)
+            .resourceChain(true);
     }
     
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
+        return container -> {
+            container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,"/notFound"));
+        };
+    }
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("forward:/index.html");
+        registry.addViewController("/notFound").setViewName("forward:/index.html");
     }
 
 }
